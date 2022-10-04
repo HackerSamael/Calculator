16afec5847e078ae27894b9ff09b3bd2bbbd3ed6
HackerSamael <114967275+HackerSamael@users.noreply.github.com>
2022-10-04 07:32:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+let screen = document.getElementById("screen");
+buttons = document.querySelectorAll("button");
+let screenValue = "";
+for(item of buttons){
+    item.addEventListener('click', (e)=>{
+        buttontext = e.target.innerText;
+        console.log("button text is ",buttontext);
+        if (buttontext == "X"){
+            buttontext = "*";
+            screenValue += buttontext;
+            screen.value = screenValue;
+
+        }
+
+        else if(buttontext == "C"){
+            screenValue = "";
+            screen.value = screenValue;
+        }
+
+        else if(buttontext == "="){
+            screenValue = eval(screenValue);
+            
+            screen.value = screenValue;
+
+        }
+
+        else if(buttontext == "Del"){
+            screenValue = screenValue.slice(0,-1)
+            screen.value = screenValue;
+        }
+        
+
+        else{
+            screenValue += buttontext;
+            screen.value = screenValue;
+        }
+
+    })
+
+}
